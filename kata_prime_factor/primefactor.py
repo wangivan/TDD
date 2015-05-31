6db24644c9ba34b2deebd718c5b5028b26e9b190
codingboy <codingboy@gmail.com>
2015-05-31 14:41:53 +0800

    first version

---
+++ kata_prime_factor/primefactor.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+
+class myTestSuite(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def prime(self, number):
+        return None 
+
+    def test_1_return_empty(self):
+        self.assertEqual(self.prime(1),[])
+
+if __name__ == "__main__":
+    unittest.main()
