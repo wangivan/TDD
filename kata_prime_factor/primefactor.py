2576eb25adbb974989ddb714574333ff159339d2
codingboy <codingboy@gmail.com>
2015-05-31 14:44:47 +0800

    finish '2 return [2]'

--- kata_prime_factor/primefactor.py
+++ kata_prime_factor/primefactor.py
@@ -10,10 +10,15 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def prime(self, number):
-        return [] 
+        if( number == 2 ):
+            return [2] 
+        return []
 
     def test_1_return_empty(self):
         self.assertEqual(self.prime(1),[])
+
+    def test_2_return_empty(self):
+        self.assertEqual(self.prime(2),[2])
 
 if __name__ == "__main__":
     unittest.main()
