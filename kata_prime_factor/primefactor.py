fc70e574557cf8d5f0b5f2c0d6d3387640e20692
codingboy <codingboy@gmail.com>
2015-05-31 14:47:47 +0800

    finish '3 return [3]' and refactor code

--- kata_prime_factor/primefactor.py
+++ kata_prime_factor/primefactor.py
@@ -10,15 +10,18 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def prime(self, number):
-        if( number == 2 ):
-            return [2] 
+        if( number > 1 ):
+            return [number]
+
         return []
 
     def test_1_return_empty(self):
         self.assertEqual(self.prime(1),[])
 
-    def test_2_return_empty(self):
+    def test_2_return_2(self):
         self.assertEqual(self.prime(2),[2])
 
+    def test_3_return_3(self):
+        self.assertEqual(self.prime(3),[3])
 if __name__ == "__main__":
     unittest.main()
