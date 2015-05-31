443769f8fb0530509f6431808612fb8a00e404b7
codingboy <codingboy@gmail.com>
2015-05-31 17:38:50 +0800

    fix flake8 issues

--- kata_prime_factor/primefactor.py
+++ kata_prime_factor/primefactor.py
@@ -1,7 +1,5 @@
 import unittest
 
-
-
 class myTestSuite(unittest.TestCase):
     def setup(self):
         pass
@@ -13,36 +11,36 @@ class myTestSuite(unittest.TestCase):
         result = []
         candidate = 2
 
-        while(candidate < number ):
-            while( number % candidate == 0 ):
+        while(candidate < number):
+            while(number % candidate == 0):
                 result.append(candidate)
-                number = number / candidate 
+                number = number / candidate
             candidate += 1
 
-        if( number > 1 ):
+        if(number > 1):
             result.append(number)
 
-        return result 
+        return result
 
     def test_1_return_empty(self):
-        self.assertEqual(self.prime(1),[])
+        self.assertEqual(self.prime(1), [])
 
     def test_2_return_2(self):
-        self.assertEqual(self.prime(2),[2])
+        self.assertEqual(self.prime(2), [2])
 
     def test_3_return_3(self):
-        self.assertEqual(self.prime(3),[3])
+        self.assertEqual(self.prime(3), [3])
 
     def test_4_return_2_2(self):
-        self.assertEqual(self.prime(4),[2,2])
+        self.assertEqual(self.prime(4), [2, 2])
 
     def test_6_return_2_3(self):
-        self.assertEqual(self.prime(6),[2,3])
+        self.assertEqual(self.prime(6), [2, 3])
 
     def test_8_return_2_2_2(self):
-        self.assertEqual(self.prime(8),[2,2,2])
+        self.assertEqual(self.prime(8), [2, 2, 2])
 
     def test_9_return_3_3(self):
-        self.assertEqual(self.prime(9),[3,3])
+        self.assertEqual(self.prime(9), [3, 3])
 if __name__ == "__main__":
     unittest.main()
