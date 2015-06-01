d2060e6a056f8f70b3fc2365c3c2df6eaa0f854a
Wei Xiao <wei.xiao@advantest.com>
2015-06-01 11:23:30 +0800

    finish 5 to V and refactor array to map

--- kata_roman_numerals/roman_numerals.py
+++ kata_roman_numerals/roman_numerals.py
@@ -8,9 +8,8 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def number2roman(self, number):
-	preset_roman = ["","I","II","III"]
+	preset_roman = {0:"",1:"I",2:"II",3:"III",5:"V"}
 	return preset_roman[number]
-
 
     def test_0_return_empty(self):
         self.assertEqual(self.number2roman(0), "")
@@ -23,5 +22,8 @@ class myTestSuite(unittest.TestCase):
 
     def test_3_return_III(self):
         self.assertEqual(self.number2roman(3), "III")
+    
+    def test_5_return_V(self):
+        self.assertEqual(self.number2roman(5), "V")
 if __name__ == "__main__":
     unittest.main()
