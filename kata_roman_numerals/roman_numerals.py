a8092755359ca8d9f4006f5b4dad976fe341b676
Wei Xiao <wei.xiao@advantest.com>
2015-06-01 13:04:49 +0800

    rewrite 3 to III and refactor code

--- kata_roman_numerals/roman_numerals.py
+++ kata_roman_numerals/roman_numerals.py
@@ -8,12 +8,11 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def number2roman(self, number):
-	if(number == 2):
-	    return "II"
-
 	preset_roman = {1:"I",5:"V",10:"X",50:"L",100:"C",500:"D"}
 	if(preset_roman.has_key(number)):
 	    return preset_roman[number]
+	if( number > 1 ):
+	    return "".ljust(number,preset_roman[1])
 	return "";
 
     def test_0_return_empty(self):
@@ -21,12 +20,6 @@ class myTestSuite(unittest.TestCase):
 
     def test_1_return_I(self):
         self.assertEqual(self.number2roman(1), "I")
-
-    def test_2_return_II(self):
-        self.assertEqual(self.number2roman(2), "II")
-
-#    def test_3_return_III(self):
-#        self.assertEqual(self.number2roman(3), "III")
 
     def test_5_return_V(self):
         self.assertEqual(self.number2roman(5), "V")
@@ -42,5 +35,12 @@ class myTestSuite(unittest.TestCase):
     
     def test_500_return_C(self):
         self.assertEqual(self.number2roman(500), "D")
+
+    def test_2_return_II(self):
+        self.assertEqual(self.number2roman(2), "II")
+
+    def test_3_return_III(self):
+        self.assertEqual(self.number2roman(3), "III")
+
 if __name__ == "__main__":
     unittest.main()
