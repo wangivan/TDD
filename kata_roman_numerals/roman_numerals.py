65d6b1b30421a175db75f0432fcaf07d7c2014f1
Wei Xiao <wei.xiao@advantest.com>
2015-06-01 11:25:06 +0800

    finish 10 to X

--- kata_roman_numerals/roman_numerals.py
+++ kata_roman_numerals/roman_numerals.py
@@ -8,7 +8,7 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def number2roman(self, number):
-	preset_roman = {0:"",1:"I",2:"II",3:"III",5:"V"}
+	preset_roman = {0:"",1:"I",2:"II",3:"III",5:"V",10:"X"}
 	return preset_roman[number]
 
     def test_0_return_empty(self):
@@ -25,5 +25,8 @@ class myTestSuite(unittest.TestCase):
     
     def test_5_return_V(self):
         self.assertEqual(self.number2roman(5), "V")
+
+    def test_10_return_X(self):
+        self.assertEqual(self.number2roman(10), "X")
 if __name__ == "__main__":
     unittest.main()
