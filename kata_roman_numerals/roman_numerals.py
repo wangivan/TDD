b674232d854007fbce872f76386575dae27e3971
Wei Xiao <wei.xiao@advantest.com>
2015-06-01 11:03:42 +0800

    finish 2 to II

--- kata_roman_numerals/roman_numerals.py
+++ kata_roman_numerals/roman_numerals.py
@@ -8,10 +8,14 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def number2roman(self, number):
+	if(number == 2):
+	    return "II"
         return "I" 
 
     def test_1_return_I(self):
         self.assertEqual(self.number2roman(1), "I")
 
+    def test_2_return_II(self):
+        self.assertEqual(self.number2roman(2), "II")
 if __name__ == "__main__":
     unittest.main()
