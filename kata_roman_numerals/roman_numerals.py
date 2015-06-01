a1a4d8d30604217003617b9514a8e80e58fef568
Wei Xiao <wei.xiao@advantest.com>
2015-06-01 11:06:14 +0800

    finish 3 to III

--- kata_roman_numerals/roman_numerals.py
+++ kata_roman_numerals/roman_numerals.py
@@ -8,6 +8,8 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def number2roman(self, number):
+	if(number == 3):
+	    return "III"
 	if(number == 2):
 	    return "II"
         return "I" 
@@ -17,5 +19,8 @@ class myTestSuite(unittest.TestCase):
 
     def test_2_return_II(self):
         self.assertEqual(self.number2roman(2), "II")
+
+    def test_3_return_III(self):
+        self.assertEqual(self.number2roman(3), "III")
 if __name__ == "__main__":
     unittest.main()
